d14f66ffed17fb0e9f15e2f83911f82cc49e3853
Vivy <Tomitaviorel@yahoo.com>
2023-03-02 10:07:21 +0200

    feat: custom errors

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,5 +1,26 @@
+import { ArrowUturnLeftIcon, HomeIcon } from '@heroicons/react/24/solid';
+import { Link, useNavigate, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  const nav = useNavigate();
+
+  return (
+    <div className='error'>
+      <h1>We've got a problem.</h1>
+      <p>{error.message || error.status.Text}</p>
+      <div className='flex-md'>
+        <button className='btn btn--dark' onClick={() => nav(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go back</span>
+        </button>
+        <Link to='/' className='btn btn--dark'>
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
