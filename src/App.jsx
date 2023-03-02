399dbbad26f7d80e769d3eca9856a02b564e129a
Vivy <Tomitaviorel@yahoo.com>
2023-03-02 09:01:16 +0200

    feat: creating first component and using loader fct

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard, { dashboardLoader } from './pages/dashboard';
+import Error from './pages/error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />,
+  },
+]);
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
